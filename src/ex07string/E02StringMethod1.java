2a2248e2d1b1ca328032eaa1fe10345dbfd14a66
southwinds97 <southwinds970521@gmail.com>
2024-05-31 17:50:54 +0900

    24년5월31일 수업완료

--- src/ex07string/E02StringMethod1.java
+++ src/ex07string/E02StringMethod1.java
@@ -14,17 +14,18 @@ public class E02StringMethod1 {
         System.out.println("str1의 길이:" + str1.length());
         System.out.println("str2의 길이:" + str2.length());
 
-        // 2] charAt(인덱스): 문자열에서 특정 index에 해당하는 문자 하나를 반환
+        // 2] charAt(인덱스): 문자열에서 특정 index에 해당하는 문자 하나를 반환. index는 0부터 시작
         System.out.println("### 2 ###");
         System.out.println("str1의 두번째 문자:" + str1.charAt(1));
         System.out.println("str2의 두번째 문자:" + str2.charAt(1));
 
-        // 3] compareTo(): 두 문자열을 비교할 때 사용하는 메소드
+        // 3] compareTo(): 2개의 문자열의 첫번째 문자부터 순차적으로 비교하면서 앞문자의 아스키코드가 크면 양수를 반환
+        // 뒷문자의 아스키코드가 크면 음수를 반환. 같으면 0을 반환
         System.out.println("### 3 ###");
         String str3 = "A";
         String str4 = "B";
-        System.out.println(str3.compareTo(str4));
-        System.out.println(str4.compareTo(str3));
+        System.out.println(str3.compareTo(str4)); // -1
+        System.out.println(str4.compareTo(str3)); // 1
         System.out.println("ABC".compareTo("ABC") == 0 ? "문자열이 같다" : "문자열이 다르다");
 
         // 4] concat(): 두 문자열을 연결할 때 사용하는 메소드
@@ -34,8 +35,8 @@ public class E02StringMethod1 {
 
         // 5] contains(): 문자열에 특정 문자열이 포함되어 있는지 판단
         System.out.println("### 5 ###");
-        System.out.println(str1.contains("To"));
-        System.out.println(str1.contains("to"));
+        System.out.println(str1.contains("To")); // false
+        System.out.println(str1.contains("to")); // true
 
     }
 
--- src/ex07string/E02StringMethod2.java
+++ src/ex07string/E02StringMethod2.java
@@ -2,51 +2,53 @@ package ex07string;
 
 public class E02StringMethod2 {
 
-    public static void main(String[] args) {
+        public static void main(String[] args) {
 
-        System.out.println("String 클래스의 주요 메소드2");
+                System.out.println("String 클래스의 주요 메소드2");
 
-        String str1 = "Welcome to java";
-        String str2 = "자바야 놀자!";
+                String str1 = "Welcome to java";
+                String str2 = "자바야 놀자!";
 
-        /*
-         * 6] codePointAt(인덱스)
-         */
-        System.out.println("### 6 ###");
+                /*
+                 * 6] codePointAt(인덱스) : 문자열에서 특정 index에 해당하는 문자의 아스키코드(혹은 유니코드) 값을 반환한다.
+                 */
+                System.out.println("### 6 ###");
+                // 대문자w : 87
+                System.out.println("str1 첫번째 문자의 아스키코드:" +
+                                str1.codePointAt(0));
+                // 한글'야' : 50556
+                System.out.println("str2 세번째 문자의 아스키코드:" +
+                                str2.codePointAt(2));
 
-        System.out.println("str1 첫번째 문자의 아스키코드:" +
-                str1.codePointAt(0));
+                /*
+                 * 7] endsWith() : 특정 문자열로 끝나면 true를 반환
+                 * 8] startsWith() : 특정 문자열로 시작하면 true를 반환
+                 */
+                System.out.println("### 7,8 ###");
+                System.out.println("www.daum.net".endsWith("net"));
+                System.out.println("naver.com".startsWith("http"));
 
-        System.out.println("str2 세번째 문자의 아스키코드:" +
-                str2.codePointAt(2));
+                /*
+                 * 9]format() : 출력형식을 지정하여 문자열로 반환하고 싶을 때 사용 printf()와 사용법이 동일하다. 주로 웹애플리케이션 제작에
+                 * 사용된다.
+                 */
+                System.out.println("### 9 ###");
+                System.out.printf("국어:%d,영어:%d,수학:%d\n",
+                                81, 92, 100);
 
-        /*
-         * 7] endsWith()
-         * 8] startsWith()
-         */
-        System.out.println("### 7,8 ###");
-        System.out.println("www.daum.net".endsWith("net"));
-        System.out.println("naver.com".startsWith("http"));
+                String formatStr = String.format("국어:%d,영어:%d,수학:%d\n",
+                                81, 92, 100);
+                System.out.println(formatStr);
 
-        /*
-         * 9]format()
-         */
-        System.out.println("### 9 ###");
-        System.out.printf("국어:%d,영어:%d,수학:%d\n",
-                81, 92, 100);
-
-        String formatStr = String.format("국어:%d,영어:%d,수학:%d\n",
-                81, 92, 100);
-        System.out.println(formatStr);
-
-        /*
-         * 10] indexOf()
-         */
-        System.out.println("### 10 ###");
-        String email1 = "hong@daum.net";
-        System.out.println(str1.indexOf("ava"));// 12
-        System.out.println(str1.indexOf("J"));// -1
-        System.out.println((email1.indexOf("@") != -1) ? "이메일형식맞음" : "이메일형식아님");
-    }
+                /*
+                 * 10] indexOf() : 문자열에서 특정 문자열의 시작인덱스를 반환한다. 만약 찾는 문자열이 없다면 -1을 반환한다.
+                 * 인덱스는 항상 0부터 시작하므로 -1은 찾는 문자열이 없다는 의미이다.
+                 */
+                System.out.println("### 10 ###");
+                String email1 = "hong@daum.net";
+                System.out.println(str1.indexOf("ava"));// 12
+                System.out.println(str1.indexOf("J"));// -1
+                System.out.println((email1.indexOf("@") != -1) ? "이메일형식맞음" : "이메일형식아님");
+        }
 
 }
